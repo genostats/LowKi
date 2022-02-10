061c7ac9f747d7a78f09e08df22c82665f2e526b
HervePerdry <herve.perdry@gmail.com>
2022-02-10 12:04:03 +0100

    +few lines in test_20.r

--- test_20.r
+++ test_20.r
@@ -12,11 +12,16 @@ K.low.20 <- lowKi(vcf.file.20, freqs = MLfreq, adjust = FALSE)
 K.real <- readRDS(kinship.file)
 K.real.20 <- readRDS(kinship.file.20)
 
+par(ask = TRUE)
 m <- match( rownames(K.low.20), rownames(K.low) )
-plot( K.low.20, K.low[m,m])
+# raw coeffs with same allelic ref are identical, as they ought to be
+plot( K.low.20, K.low[m,m]) ; abline(0,1,col="red")
 
 K.low.adj <- lowKi(vcf.file, freqs = MLfreq, adjust = TRUE)
 K.low.adj.20 <- lowKi(vcf.file.20, freqs = MLfreq, adjust = TRUE)
+plot( K.low.adj.20, K.low.adj[m,m]); abline(0,1,col="red")
 
-plot( K.low.adj.20, K.low.adj[m,m])
+K.low.adj.noref <- lowKi(vcf.file, adjust = TRUE)
+K.low.adj.noref.20 <- lowKi(vcf.file.20, adjust = TRUE)
+plot( K.low.adj.noref.20, K.low.adj.noref[m,m]); abline(0,1,col="red")
 
